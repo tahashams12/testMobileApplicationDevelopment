31048b1aed9d230ceac51c628681388911986450
TahaShams <tahaalishams@gmail.com>
2025-04-03 12:51:18 +0500

    lab 1 done

---
+++ lab1/lib/task1/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class testWidget extends StatelessWidget {
+  const testWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lab1/lib/task2/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class testWidget extends StatefulWidget {
+  const testWidget({super.key});
+
+  @override
+  State<testWidget> createState() => _testWidgetState();
+}
+
+class _testWidgetState extends State<testWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
