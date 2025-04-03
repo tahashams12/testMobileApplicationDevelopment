59d2de85db4e9fabf42fb972a0373c7efd649bae
TahaShams <tahaalishams@gmail.com>
2025-04-03 13:06:53 +0500

    my lab 2 is done

---
+++ lab2/lib/task1/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class checkTask2W extends StatelessWidget {
+  const checkTask2W({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lab2/lib/task2/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CHECKtASKd extends StatefulWidget {
+  const CHECKtASKd({super.key});
+
+  @override
+  State<CHECKtASKd> createState() => _CHECKtASKdState();
+}
+
+class _CHECKtASKdState extends State<CHECKtASKd> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
